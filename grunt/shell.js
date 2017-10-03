430ba09a679ea791b656a67428582c20315af2b5
REPTILEHAUS <paddy.osullivan@reptileha.us>
2017-10-03 16:17:51 +0200

    shell commands added

--- grunt/shell.js
+++ grunt/shell.js
@@ -2,13 +2,41 @@
 
 var REPTILEHAUS = require('../grunt.config.json');
 
-module.exports = { 
+function log(err, stdout, stderr, cb) {
+    if (err) {
+        cb(err);
+        return;
+    }
+    console.log(stderr); 
+    console.log(err);     
+    console.log(stdout);
+    cb();
+}
 
-    start: {
-        command: ["bower install"].join('&&')
+module.exports = { 
+    firsttime: {
+        command: [ "sudo npm install -g bower-update-all",
+                   "sudo npm install -g npm-check-updates",
+                   "sudo npm install -g bower", 
+                   "sudo npm install -g grunt-cli",                 
+                 ].join('&&'),
+        options: {
+            callback: log
+        }
+    },
+    updates: {
+        command: [ "npm-check-updates -u --packageFile package.json",
+                   "bower-update-all"
+                ].join('&&'),
+        options: {
+            callback: log
+        }
     },
     wp_regenerate: {
-        command: 'wp media regenerate --url=' + REPTILEHAUS.shell.dev_url + ' --yes --path=' + REPTILEHAUS.shell.dev_path
+        command: 'wp media regenerate --url=' + REPTILEHAUS.shell.dev_url + ' --yes --path=' + REPTILEHAUS.shell.dev_path,
+        options: {
+            callback: log
+        }
     }
 
 };
